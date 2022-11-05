969872183045b5bb41499aadb9f3e047a18d0c66
USER <USER@DESKTOP-0OAL19H>
2022-11-05 09:23:31 +0900

    1105

--- greenApple/src/main/java/com/ga/greenApple/controller/OrderController.java
+++ greenApple/src/main/java/com/ga/greenApple/controller/OrderController.java
@@ -16,9 +16,6 @@ public class OrderController {
 	@Autowired
 	private OrderService os;
 	
-	@Autowired
-	private MemberService ms;
-	
 	// 회원별 주문 목록
 	@PostMapping(value = "/order/memberOrderList")
 	public List<Order> memOrderList(@RequestBody String id) {
@@ -27,5 +24,13 @@ public class OrderController {
 		return memberOrderList;
 	}
 	
+	// 장바구니 - 확인 - 결제 - 주문 완료
+	@PostMapping(value = "/order/payment")
+	public int payment(@RequestBody Order order) {
+		int result = 0;
+		
+		return result;
+	}
 	
+	// 바로 구매 - 확인 - 결제 - 주문 완료
 }
