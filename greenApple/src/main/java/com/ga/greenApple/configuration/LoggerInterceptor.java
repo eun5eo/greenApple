b292403f4caed4753a82597ed614f01387f8de29
eun5eo <dms212701@gmail.com>
2022-12-05 10:41:08 +0900

    1205 (1) - session 관련 수정

---
+++ greenApple/src/main/java/com/ga/greenApple/configuration/LoggerInterceptor.java
@@ -0,0 +1,33 @@
+package com.ga.greenApple.configuration;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import lombok.extern.slf4j.Slf4j;
+
+// @Slf4j : 로깅 추상화 라이브러리
+// 로깅 추상화 : 로깅을 직접 하지 않고 로깅 구현체를 찾아 기능을 사용할 수 있게 해준다
+@Slf4j
+public class LoggerInterceptor implements HandlerInterceptor {
+
+    @Override
+    // 컨트롤러의 메소드에 매핑된 특정 URI가 호출됐을 때 실행되는 메서드로, 컨트롤러를 경유(접근)하기 직전에 실행
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        log.debug("===============================================");
+        log.debug("==================== BEGIN ====================");
+        log.debug("Request URI ===> " + request.getRequestURI());
+        return HandlerInterceptor.super.preHandle(request, response, handler);
+    }
+
+    @Override
+    // 컨트롤러를 경유(접근) 한 후, 즉 화면(View)으로 결과를 전달하기 전에 실행되는 메소드
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        log.debug("==================== END ======================");
+        log.debug("===============================================");
+        HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
+    }
+
+}
--- greenApple/src/main/java/com/ga/greenApple/configuration/WebMvcConfiguration.java
+++ greenApple/src/main/java/com/ga/greenApple/configuration/WebMvcConfiguration.java
@@ -9,6 +9,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.ga.greenApple.service.SessionCheck;
 
+// WebMvcConfigurer : 이 인터페이스를 구현하면 @EnableWebMvc의 자동 설정을 베이스로 가진다
+// 원하는 설정을 추가할 수 있다는 장점이 있다 (Override 가능)
+
 @Configuration
 public class WebMvcConfiguration implements WebMvcConfigurer {
 	// spring의 root-context.xml과 servlet-context.xml 내용
@@ -26,16 +29,24 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
 	// session check
 	/* sessonCheck는 InterceptorRegistry를 재정의 한 것이니 메소드명을
 	 addInterceptors로 꼭 적어줘야한다 */
+	// addInterceptors : 애플리케이션 내 인터셉터를 등록해주는 기능
 	@Override
 	public void addInterceptors(InterceptorRegistry registory) {
 		registory.addInterceptor(new SessionCheck())
+			// excludePathPatterns : 이곳에 지정된 URI나 경로는 인터셉터 호출에서 제외 => 정적 파일 무시
 			.excludePathPatterns(
 					"/product", "/product/seasonal/**", "/product/view/**",
 					"/join", "/join/idCheck", "/login", "/member/session",
 					"/review/list/**", "/review/imgList", "/review/reviewNum"
 					)
-			// 위 지정한 것들 제외하여 모두 세션 체크를 한다
-			.addPathPatterns("/**");
+			// 인터셉터 호출에서 경로를 허용 (세션 체크를 하겠다)
+			.addPathPatterns(
+					"/product/nowOrder", 
+					"/member/information", "/member/update", "/member/delete",
+					"/member/myReview", "/logout",
+					"/review/insert", "/review/update", "/review/delete",
+					"/cart/**", "/order/**", "/admin/**"
+					);
 	}
 	
 	// spring 암호화
