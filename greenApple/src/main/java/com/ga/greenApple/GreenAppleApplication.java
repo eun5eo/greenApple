1993a7ccff386c1f7b5284d32d030b1014a546b6
eun5eo <dms212701@gmail.com>
2022-12-13 14:18:30 +0900

    1213 - nginx 오류 해결방안 테스트1

--- greenApple/src/main/java/com/ga/greenApple/GreenAppleApplication.java
+++ greenApple/src/main/java/com/ga/greenApple/GreenAppleApplication.java
@@ -2,7 +2,9 @@ package com.ga.greenApple;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan(basePackages = {"com.ga.greenApple.controller"})
 @SpringBootApplication
 public class GreenAppleApplication {
 
