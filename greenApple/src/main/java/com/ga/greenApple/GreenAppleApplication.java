80acca5db059d8ceb83881f23af4e72d5c89ce81
eun5eo <dms212701@gmail.com>
2022-12-13 14:23:47 +0900

    1213 - nginx 시도2

--- greenApple/src/main/java/com/ga/greenApple/GreenAppleApplication.java
+++ greenApple/src/main/java/com/ga/greenApple/GreenAppleApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-@ComponentScan(basePackages = {"com.ga.greenApple.controller"})
+@ComponentScan(basePackages = "com.ga.greenApple.controller")
 @SpringBootApplication
 public class GreenAppleApplication {
 
