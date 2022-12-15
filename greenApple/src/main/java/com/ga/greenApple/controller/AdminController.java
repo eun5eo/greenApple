a8115d91ebb47062fb336e4201c7f11779321685
eun5eo <dms212701@gmail.com>
2022-12-15 11:08:46 +0900

    1

--- greenApple/src/main/java/com/ga/greenApple/controller/AdminController.java
+++ greenApple/src/main/java/com/ga/greenApple/controller/AdminController.java
@@ -98,7 +98,7 @@ public class AdminController {
 		int result = 0;
 		
 		String id = (String) session.getAttribute("id");
-		String realPath = "build/resources/main/static/pdImages";
+		String realPath = "buil/resources/main/static/pdImages";
 		
 		// productCode 생성
 		Date date = new Date();
